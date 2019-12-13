7c8efef9a84eb9e868daba10a3e59d6a9d8f2f68
hardecx <31935467+hardecx@users.noreply.github.com>
2019-12-13 16:24:07 +0100

    feature for company

--- src/app/@component/setting/company/company.component.ts
+++ src/app/@component/setting/company/company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-company',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company.component.scss']
 })
 export class CompanyComponent implements OnInit {
-
-  constructor() { }
+  optionsSelect: Array<any>;
+  show: Boolean=false;
+  constructor(private _location: Location) { }
 
   ngOnInit() {
-  }
+    this.optionsSelect = [
+      { value: '1', label: 'Male' },
+      { value: '2', label: 'Female' },
+      ];
+    }
 
+  back () {
+    this._location.back()
+  }
+  switch(){
+    this.show = true;
+  }
 }
