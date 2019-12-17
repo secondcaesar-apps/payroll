e1797b7e8fe0ab29225c0d90ae71dd169222ea4e
es404020 <es404020@gmail.com>
2019-12-17 12:31:52 +0100

    add role

--- src/app/@component/setting/role/role.component.ts
+++ src/app/@component/setting/role/role.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { APIENUM } from 'src/app/@shared/enum';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { ApiserviceService } from 'src/app/@shared/apiservice.service';
 
 @Component({
   selector: 'app-role',
@@ -8,11 +11,49 @@ import { Router } from '@angular/router';
 })
 export class RoleComponent implements OnInit {
 
-  constructor(private router: Router,) { }
+  Role:FormGroup;
+  error:any;
+  success:any;
+  constructor(
+    private _fb:FormBuilder,
+    private Api:ApiserviceService
+  ) { }
 
   ngOnInit() {
+
+    this.Role= this._fb.group({
+      RoleName:['',[Validators.required]],
+      Description:['',[Validators.required]]
+    });
   }
-  NavCreateRole(){
-    this.router.navigate(['/main/settings/roles-create'])
+  createRole(){
+    this.Role.disable();
+     let value = {Status:"Active",...this.Role.value};
+ 
+     this.Api.Create(APIENUM.ROLE,value).subscribe((res:any)=>{
+        this.success=res.message
+ 
+     },err=>{
+       this.error=err.error.message;
+       this.Role.enable();
+ 
+ 
+     },()=>{
+       setTimeout(()=>{
+         this.success='';
+         this.error='';
+         this.Role.reset();
+         this.Role.enable();
+       },500)
+     })
+   }
+
+   get RoleName(){
+    return this.Role.get('RoleName');
   }
+  get Description(){
+    return this.Role.get('Description');
+  }
+
+
 }
