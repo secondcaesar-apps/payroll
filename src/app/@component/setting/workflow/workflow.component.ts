95d12c89ca3dac57db44b2038d81b9fe6466baa1
es404020 <es404020@gmail.com>
2020-01-13 16:38:55 +0100

    workflow

--- src/app/@component/setting/workflow/workflow.component.ts
+++ src/app/@component/setting/workflow/workflow.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit,  ViewChild, HostListener  } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { ApiserviceService } from 'src/app/@shared/apiservice.service';
 import { APIENUM } from 'src/app/@shared/enum';
 import { MdbTableDirective } from 'ng-uikit-pro-standard';
@@ -11,52 +11,68 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./workflow.component.scss']
 })
 export class WorkflowComponent implements OnInit {
-  
+
   @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
   elements = [];
   searchText: string = '';
   previous: string;
-  message: Boolean=false;
-  loading:Boolean=true;
+  message: Boolean = false;
+  loading: Boolean = true;
   messages: string;
-  WorkFlow:FormGroup;
-  error:any;
-  success:any;
-  emp:any;
+  WorkFlow: FormGroup;
+  error: any;
+  success: any;
+  emp: any;
   constructor(
-    private _fb:FormBuilder,
-    private Api:ApiserviceService
+    private _fb: FormBuilder,
+    private Api: ApiserviceService
   ) { }
 
   ngOnInit() {
     this.Api.Read(APIENUM.WORKFLOWSETUP)
-    .subscribe((res:any)=>{
-      this.loading = false;
-      this.elements=res.records;
-      this.mdbTable.setDataSource(this.elements);
-      this.elements = this.mdbTable.getDataSource();
-      this.previous = this.mdbTable.getDataSource();
-    }, (err: any) => {
-      this.loading = false;
-      this.messages = err.error.message;
-      this.message = true;
-    })
+      .subscribe((res: any) => {
+        this.loading = false;
+        this.elements = res.records;
+        this.mdbTable.setDataSource(this.elements);
+        this.elements = this.mdbTable.getDataSource();
+        this.previous = this.mdbTable.getDataSource();
+      }, (err: any) => {
+        this.loading = false;
+        this.messages = err.error.message;
+        this.message = true;
+      })
     this.Api.Read(APIENUM.EMP)
-    .pipe(
-      map((res:any)=>res.records)
-    )
-    .subscribe((res:any)=>{
-      console.log(res);
-      this.emp=res;
-    });
+      .pipe(
+        map((res: any) => res.records)
+      )
+      .subscribe((res: any) => {
+        console.log(res);
+        this.emp = res;
+      });
 
-    this.WorkFlow= this._fb.group({
-      PWFName:['',[Validators.required]],
-      EmployeeID:['',[Validators.required]],
-      level:['',[Validators.required]],
+    this.WorkFlow = this._fb.group({
     
+      Item:this._fb.array([this.createWorkflows()]),
+
     });
   }
+  createWorkflows(): FormGroup{
+    return this._fb.group({
+      PWFName: ['', ],
+      EmployeeID: ['',],
+      level: [''],
+      Status: ["Active"]
+      
+
+    });
+  };
+  get itemArray() {
+    return this.WorkFlow.get('Item') as FormArray;
+}
+  addItem(){
+    this.itemArray.push(this.createWorkflows());
+    }
+  
   searchItems() {
     const prev = this.mdbTable.getDataSource();
 
@@ -70,42 +86,54 @@ export class WorkflowComponent implements OnInit {
       this.mdbTable.setDataSource(prev);
     }
   }
-  get PWFName(){
+  get PWFName() {
     return this.WorkFlow.get('PWFName');
   }
 
-  
-  get EmployeeID(){
+
+  get EmployeeID() {
     return this.WorkFlow.get('EmployeeID');
   }
-///testinng
-  
-  get level(){
+  ///testinng
+
+  get level() {
     return this.WorkFlow.get('level');
   }
 
-  createWorkflow(){
-    this.WorkFlow.disable();
-    let value = {Status:"Active",...this.WorkFlow.value};
-    this.Api.Create(APIENUM.WORKFLOWSETUP,value).subscribe((res:any)=>{
-      this.success=res.message
-
-   },err=>{
-     this.error=err.error.message;
-     this.WorkFlow.enable();
+  createWorkflow() {
    
+    this.WorkFlow.disable();
+   
+    this.Api.Create(APIENUM.WORKFLOWSETUP,this.WorkFlow.value['Item']).subscribe((res: any) => {
+      this.success = res.message
 
-   },()=>{
-     setTimeout(()=>{
-       this.success='';
-       this.error='';
-       this.WorkFlow.reset();
-       this.WorkFlow.enable();
-     },500)
+    }, err => {
+      this.error = err.error.message;
+      this.WorkFlow.enable();
 
- 
-   })
 
+    }, () => {
+      setTimeout(() => {
+        this.success = '';
+        this.error = '';
+        this.WorkFlow.reset();
+        this.WorkFlow.enable();
+        
+    this.WorkFlow = this._fb.group({
+     
+      Item:this._fb.array([this.createWorkflows()]),
+
+    });
+      }, 500)
+
+
+    })
+
+  }
+
+  removeItems(id) {
+    this.itemArray.removeAt(id);
+   // this.Cart();
   }
 
 }
