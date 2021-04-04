da721127ad796c4ac7c5226984bbc3e1b0e4453d
es404020 <es404020@gmail.com>
2021-04-04 05:22:19 +0100

    ok fix bug

--- src/app/@component/setting/analystic/analystic.component.ts
+++ src/app/@component/setting/analystic/analystic.component.ts
@@ -4,21 +4,22 @@ import { APIENUM } from 'src/app/@shared/enum';
 import { SharedService } from 'src/app/@shared/shared/shared.service';
 import { ColumnSetting } from 'src/app/models/layout.model';
 import { BaseComponent } from '../../base/base.component';
-
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-analystic',
   templateUrl: './analystic.component.html',
   styleUrls: ['./analystic.component.scss']
 })
 export class AnalysticComponent extends BaseComponent implements OnInit {
-
-  routePage ="../edit";
-  apis='readbyperiod';
-  q = ['Q1','Q2','Q3','Q4'];
+  passDate: any;
+  routePage = "../edit";
+  apis = 'readbyperiod';
+  q = ['Q1', 'Q2', 'Q3', 'Q4'];
 
   dates = new Date().getFullYear();
-  year: any= this.dates;
-  D = [this.dates-2,this.dates-1,this.dates,this.dates+1,this.dates+2];
+  year: any = this.dates;
+  D = [this.dates - 2, this.dates - 1, this.dates, this.dates + 1, this.dates + 2];
   projectSettings: ColumnSetting[] = [
 
     {
@@ -30,7 +31,8 @@ export class AnalysticComponent extends BaseComponent implements OnInit {
     {
       primaryKey: "Rating",
       header: "Rating",
-      routerParams:true,
+      percent:true,
+      routerParams: true,
 
 
 
@@ -44,10 +46,11 @@ export class AnalysticComponent extends BaseComponent implements OnInit {
 
   ];
   QT: any;
+  loadings: boolean;
 
 
 
-  constructor(private shared: SharedService,public api: ApiserviceService){
+  constructor(private shared: SharedService, public api: ApiserviceService) {
 
     super(api);
   }
@@ -56,45 +59,63 @@ export class AnalysticComponent extends BaseComponent implements OnInit {
 
   }
 
-  getPeriod(id){
+  getPeriod(id) {
 
-    this.shared.getInfo().subscribe((res)=>{
+    this.shared.getInfo().subscribe((res) => {
 
-      if(res){
-        this.special(APIENUM.SQR,id);
+      if (res) {
+        this.special(APIENUM.SQR, id);
       }
     })
-    this.special(APIENUM.SQR,id);
+    this.special(APIENUM.SQR, id);
 
   }
 
-  qselect(event,i){
-    let val= event.target.value;
-            if(i=='Q'){
+  qselect(event, i) {
+    let val = event.target.value;
+    if (i == 'Q') {
 
-              this.QT=val;
-
-
-            }else{
-
-               this.year=val;
+      this.QT = val;
 
 
-            }
+    } else {
 
-            if(this.QT!=null && this.year!=null){
-
-              let result = this.QT+"-"+this.year;
-
-              let value= {"Period": result};
-              this.baseItems=null;
-              this.getPeriod(value);
-            }
+      this.year = val;
 
 
-           // console.log(event.target.value);
+    }
 
-          }
+    if (this.QT != null && this.year != null) {
+
+      let result = this.QT + "-" + this.year;
+      this.passDate = result;
+      let value = { "Period": result };
+      this.baseItems = null;
+      this.getPeriod(value);
+    }
+
+
+    // console.log(event.target.value);
+
+  }
+  public captureScreen()
+  {
+     this.loadings = true;
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+       // Few necessary setting options
+        var imgWidth = 200;
+        var pageHeight = 255;
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+        const contentDataURL = canvas.toDataURL('image/png')
+        let pdf = new jspdf('p', 'mm', 'a4');  // A4 size page of PDF
+         var position = 0;
+          pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+          pdf.save('pdf.pdf'); // Generated PDF
+          this.loadings = false;
+         });
+  }
 
 }
 // DateCreated: "2021-03-18 12:16:13"
--- src/app/@component/setting/feeback/feeback.component.ts
+++ src/app/@component/setting/feeback/feeback.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  Input, ChangeDetectionStrategy, OnChanges } from '@angular/core';
 import { ApiserviceService } from 'src/app/@shared/apiservice.service';
 import { APIENUM } from 'src/app/@shared/enum';
 import { SharedService } from 'src/app/@shared/shared/shared.service';
@@ -8,10 +8,11 @@ import { BaseComponent } from '../../base/base.component';
 @Component({
   selector: 'app-feeback',
   templateUrl: './feeback.component.html',
-  styleUrls: ['./feeback.component.scss']
-})
-export class FeebackComponent extends BaseComponent implements OnInit {
+  styleUrls: ['./feeback.component.scss'],
 
+})
+export class FeebackComponent extends BaseComponent implements OnChanges {
+  @Input() dateValue: any =null;
   routePage ="../edit";
   apis=APIENUM.SVP;
   q = ['Q1','Q2','Q3','Q4'];
@@ -53,9 +54,29 @@ export class FeebackComponent extends BaseComponent implements OnInit {
     super(api);
   }
 
-  ngOnInit() {
+  ngOnChanges()  {
+
+
+
+
+    if(this.dateValue!=null){
+// let result = this.QT+"-"+this.year;
+
+    let value= {"Period": this.dateValue};
+    this.baseItems=null;
+    this.getPeriod(value);
+    }
+
+
+
+    // let result = this.QT+"-"+this.year;
+
+    // let value= {"Period": result};
+    // this.baseItems=null;
+    // this.getPeriod(value);
 
   }
+
 
   getPeriod(id){
 
@@ -69,33 +90,7 @@ export class FeebackComponent extends BaseComponent implements OnInit {
 
   }
 
-  qselect(event,i){
-    let val= event.target.value;
-            if(i=='Q'){
 
-              this.QT=val;
-
-
-            }else{
-
-               this.year=val;
-
-
-            }
-
-            if(this.QT!=null && this.year!=null){
-
-              let result = this.QT+"-"+this.year;
-
-              let value= {"Period": result};
-              this.baseItems=null;
-              this.getPeriod(value);
-            }
-
-
-           // console.log(event.target.value);
-
-          }
 
 }
 // DateCreated: "2021-03-18 12:16:13"
